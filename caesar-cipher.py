511c9e686ba6b09c3fd76cdd772c23baf2e15b04
Zack Sangem <zacksangem797@gmail.com>
2025-12-19 05:47:18 +0530

    part 2 done

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -5,8 +5,6 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-
 def encrypt(original_text, shift_amount):
     cipher_text = ""
 
@@ -16,14 +14,26 @@ def encrypt(original_text, shift_amount):
         new_position %= len(alphabet)
         cipher_text += alphabet[new_position]
     
-    print(cipher_text)   
+    print(cipher_text)
 
-encrypt(text , shift)
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
 
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
 
+def decrypt(original_text, shift_amount):
+    cipher_text = ""
+
+    for letter in original_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_position %= len(alphabet)
+        cipher_text += alphabet[new_position]
+
+    print(cipher_text)
+
+ 
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
+else:
+    print("Invalid choice. Type 'encode' or 'decode'.")
